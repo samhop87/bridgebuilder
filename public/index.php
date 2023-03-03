f5b0c53eb0cb59316ba2265935edeadea447212c
Sam Hopkinson <s.hopkinson87@gmail.com>
2023-03-03 17:16:22 +0000

    Added basic ci/cd script

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'This is bridgebuilder; it will determine the best possible bridge across a body of water.
+The start/end points are randomly generated, and the bridge is built using an algorithm that determines best value for cost.';
